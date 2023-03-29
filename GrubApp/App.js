bf558d92c7d232e829b4f1bd4af0995466ccb699
Alan Hunt <hja7333@gmail.com>
2023-03-29 15:10:36 +0100

    deleted files and changed app.js

--- GrubApp/App.js
+++ GrubApp/App.js
@@ -1,30 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Text } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import { Login } from "./components/Login";
-import { CreateAccount } from "./components/CreateAccount";
 
-const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="CreateAccount" component={CreateAccount} />
-      </Stack.Navigator>
-    </NavigationContainer>
-
-    // <View style={styles.container}>
-    //   <StatusBar style="auto" />
-    // </View>
+    <View style={styles.container}>
+      <Text>Welcome to Grub</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- GrubApp/components/CreateAccount.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text, StatusBar } from "react-native";
-export const CreateAccount = ({ navigation }) => {
-  return (
-    <View>
-      <Text>Create your account here</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
