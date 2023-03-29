bdce6e8a3bfb334b15035ec359a84584da6e9541
Alan Hunt <hja7333@gmail.com>
2023-03-29 14:45:55 +0100

    added component files created login and create account

---
+++ GrubApp/components/CreateAccount.jsx
@@ -0,0 +1,9 @@
+import { View, Text, StatusBar } from "react-native";
+export const CreateAccount = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Create your account here</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
