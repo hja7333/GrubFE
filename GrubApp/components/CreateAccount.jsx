7eda63529936bb53a87b15b0cb3e27f4eb3f0c3a
James Milmine <jamesmilmine@mac.com>
2023-03-30 09:15:47 +0100

    create user context and login BE query

--- GrubApp/components/CreateAccount.jsx
+++ GrubApp/components/CreateAccount.jsx
@@ -3,7 +3,7 @@ import { View, Text, StatusBar } from "react-native";
 export const CreateAccount = ({ navigation }) => {
   return (
     <View>
-      <Text>Create your account here</Text>
+      <Text>Create your account here {global.token}</Text>
       <StatusBar style="auto" />
     </View>
   );
