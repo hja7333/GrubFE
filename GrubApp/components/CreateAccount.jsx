4e109c65188aeb1d924711af759344f63f6e2312
Alan Hunt <hja7333@gmail.com>
2023-03-30 09:30:17 +0100

    added start of account creation form

--- GrubApp/components/CreateAccount.jsx
+++ GrubApp/components/CreateAccount.jsx
@@ -1,9 +1,37 @@
-import { View, Text, StatusBar } from "react-native";
+import { View, Button, TextInput, StatusBar } from "react-native";
+import { Formik } from "formik";
 
-export const CreateAccount = ({ navigation }) => {
+export const CreateAccount = () => {
   return (
     <View>
-      <Text>Create your account here</Text>
+      <View>
+        <Formik
+          initialValues={{ username: "", Address: "", password: "" }}
+          onSubmit={(values) => {}}
+        >
+          {(props) => (
+            <View>
+              <TextInput
+                placeholder="username"
+                onChangeText={props.handleChange("username")}
+              />
+              <TextInput
+                placeholder="address"
+                onChangeText={props.handleChange("address")}
+              />
+              <TextInput
+                placeholder="password"
+                onChangeText={props.handleChange("password")}
+              />
+              <TextInput
+                placeholder="confirm password"
+                onChangeText={props.handleChange("confirm password")}
+              />
+              <Button title="submit" onPress={props.handleSubmit}></Button>
+            </View>
+          )}
+        </Formik>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
