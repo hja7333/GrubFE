b57392e21e076c063d9acf0eb243922c9e37bf6f
Yusef <Yusef@Yusefs-Laptop.local>
2023-03-31 12:04:26 +0100

    map has been rendered on screen

--- GrubApp/api.js
+++ GrubApp/api.js
@@ -1,28 +1,47 @@
-import axios from 'axios'; 
-import { UserContext } from './contexts/UserContext';
-import React, { useContext } from "react";
+import axios from "axios";
 
 export const getLocationDetails = (location) => {
- return axios.
-    get("https://maps.googleapis.com/maps/api/geocode/json", {
-        params: {"address": location,"key": "AIzaSyDbifXH9H07fHVrciISF08USUoW2Zg-oXo"}
-    }).then(({data: {results}}) => {
-        console.log(results[0].geometry.location)
-        return results[0].geometry.location
+  return axios
+    .get("https://maps.googleapis.com/maps/api/geocode/json", {
+      params: {
+        address: location,
+        key: "AIzaSyDbifXH9H07fHVrciISF08USUoW2Zg-oXo",
+      },
     })
-}
+    .then(({ data: { results } }) => {
+      return results[0].geometry.location;
+    });
+};
 
 export const createUser = (newUser) => {
-    return axios.post("https://grub-group-project.onrender.com/api/users", newUser).then(({data}) => {
-        return data.user;
-    })
-}
+  return axios
+    .post("https://grub-group-project.onrender.com/api/users", newUser)
+    .then(({ data }) => {
+      return data.user;
+    });
+};
 
-export const getUser = (username) => {
-    const { user } = useContext(UserContext);
-    return axios.get(`https://grub-group-project.onrender.com/api/users/${username}`, {headers: {Authorization: `Bearer ${user.token}`}}).then((res) => {
-        console.log(res)
-    })
-}
+export const getUser = (user) => {
+  return axios
+    .get(
+      `https://grub-group-project.onrender.com/api/users/${user.user.username}`,
+      {
+        headers: { Authorization: `Bearer ${user.token}` },
+      }
+    )
+    .then(({ data }) => {
+      const { user } = data;
+      return user;
+    });
+};
 
-getUser("John34")
+export const fetchAllItems = (token) => {
+  return axios
+    .get("https://grub-group-project.onrender.com/api/items", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then(({ data }) => {
+      const { items } = data;
+      return items;
+    });
+};
--- GrubApp/components/MapScreen.jsx
+++ GrubApp/components/MapScreen.jsx
@@ -1,32 +1,60 @@
-import { View, Text, StatusBar } from "react-native";
-import React, { useContext } from "react";
+import { View, Text, StatusBar, StyleSheet } from "react-native";
+import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../contexts/UserContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import MapView from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { fetchAllItems, getUser } from "../api";
 
 export const MapScreen = ({ navigation }) => {
   const { user } = useContext(UserContext);
-  console.log(user)
-  const [userLocation, setUserLocation] = useState({})
-  // const [region, setRegion] = useState({
-  //   latitude: 53.4,
-  //   longitude: -2.983,
-  //   latitudeDelta: 0.0922,
-  //   longitudeDelta: 0.0421,
-  // });
+  const [region, setRegion] = useState({});
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getUser(user).then((responseUser) => {
+      setRegion({
+        latitude: responseUser.location.latitude,
+        longitude: responseUser.location.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    });
+    fetchAllItems(user.token).then((itemsResponse) => {
+      setItems(itemsResponse);
+    });
+  }, [user]);
 
   AsyncStorage.getItem("GRUB_APP::USER_DETAILS").then((user) =>
-    console.log(Object.keys(user))
+    console.log(user)
   );
 
   return (
     <View>
-      <Text> This is the map </Text>
+      <MapView
+        initialRegion={region}
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        zoomControlEnabled={true}
+        zoomEnabled={true}
+      >
+        {items.map((item) => {
+          return (
+            <Marker
+              key={item._id}
+              title={item.name}
+              description={item.description}
+              coordinate={item.location}
+            ></Marker>
+          );
+        })}
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
 };
-
-
-//John34
-//Bananas1995
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
