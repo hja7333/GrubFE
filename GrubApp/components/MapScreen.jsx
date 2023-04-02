de735d87232440dbcecee26dbeeb721ede87cb7e
saragagg <saragaggino@gmail.com>
2023-04-02 21:01:00 +0100

    Getting user's current location via expo location

--- GrubApp/components/MapScreen.jsx
+++ GrubApp/components/MapScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StatusBar, StyleSheet} from "react-native";
+import { View, Text, StatusBar, StyleSheet, TouchableOpacity } from "react-native";
 import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../contexts/UserContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -6,14 +6,29 @@ import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { fetchAllItems, getLocationDetails, getUser } from "../api";
 import { ItemMarker } from "./ItemMarker";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-// import Geolocation from '@react-native-community/geolocation';
+import * as Location from 'expo-location';
 
-// Geolocation.setRNConfiguration(config);
 
 export const MapScreen = ({ navigation }) => {
   const { user } = useContext(UserContext);
   const [region, setRegion] = useState({});
   const [items, setItems] = useState([]);
+
+  AsyncStorage.getItem("GRUB_APP::USER_DETAILS").then((user) =>
+    console.log(user)
+  );
+
+  console.log(region)
+
+  const handleUserLocation = () => {
+    Location.requestForegroundPermissionsAsync().then((permissionResponse) => {
+      Location.getCurrentPositionAsync().then((position) => {
+        setRegion((currentRegion) => {
+          return {...currentRegion, latitude: position.coords.latitude, longitude: position.coords.longitude}
+        })
+      })
+    }).catch((err) => console.log(err))
+  }
 
   useEffect(() => {
     getUser(user).then((responseUser) => {
@@ -31,9 +46,6 @@ export const MapScreen = ({ navigation }) => {
       .catch(() => navigation.navigate("Login"));
   }, [user]);
 
-  AsyncStorage.getItem("GRUB_APP::USER_DETAILS").then((user) =>
-    console.log(user)
-  );
 
   return (
     <View>
@@ -44,33 +56,39 @@ export const MapScreen = ({ navigation }) => {
           zoomControlEnabled={true}
           zoomEnabled={true}
           style={styles.map}>
-          {items.map((item) => {
-            return (
-              <ItemMarker
-                key={item._id}
-                item={item}
-              />
-            );
-          })}
+           {/* {items.map((item) => {
+            return <ItemMarker key={item._id} item={item} />;
+          })}  */}
         </MapView>
         <View style={styles.searchContainer}>
-    <GooglePlacesAutocomplete
-      placeholder='Search location...'
-      onPress={(data, details = null) => {
-        getLocationDetails(data.description).then((locationDetails) => {
-          setRegion((currentRegion) => {
-            return {...currentRegion, latitude: locationDetails.lat, longitude: locationDetails.lng}
-          })
-        })
-      }}
-      // currentLocation={true}
-      // currentLocationLabel='Select current location'
-      query={{
-        key: 'AIzaSyDbifXH9H07fHVrciISF08USUoW2Zg-oXo',
-        language: 'en',
-      }}
-    />   
-    </View>
+          <GooglePlacesAutocomplete
+            placeholder="Search location..."
+            autoFillOnNotFound={true}
+            onPress={(data, details = null) => {
+              getLocationDetails(data.description).then((locationDetails) => {
+                setRegion((currentRegion) => {
+                  return {
+                    ...currentRegion,
+                    latitude: locationDetails.lat,
+                    longitude: locationDetails.lng,
+                  };
+                });
+              });
+            }}
+            query={{
+              key: "AIzaSyDbifXH9H07fHVrciISF08USUoW2Zg-oXo",
+              language: "en",
+            }}
+          />
+      <View><TouchableOpacity
+          style={styles.userLocationBtn}
+        onPress={handleUserLocation}
+      >
+          <Text style={{ color: "#fff", fontSize: 12 }}>Current location</Text>
+      </TouchableOpacity></View>
+        </View>     
+       
+        
         <StatusBar style="auto" />
       </View>
     </View>
@@ -88,12 +106,24 @@ const styles = StyleSheet.create({
     // flex:1
   },
   searchContainer: {
-    position: 'absolute', 
-    top: 10, 
-    width: '95%',
+    position: "absolute",
+    top: 10,
+    width: "95%",
     backgroundColor: "white",
     elevation: 3,
-    padding: 4, 
-    borderRadius: 8, 
+    padding: 4,
+    paddingBottom: 1,
+    marginBottom: 2,
+    borderRadius: 8,
   },
+  userLocationBtn: {
+    position: "absolute",
+    marginTop: 3.5,
+    borderRadius: 15,
+    backgroundColor: "#334bd6",
+    width: 150,
+    height: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  }, 
 });
